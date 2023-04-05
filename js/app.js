fa204c1be541be54e225dbc5951011703194cf2d
faisalakonda <faisalakonda2@gmail.com>
2023-04-05 16:20:38 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const loadPhones = async (searchText) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhones(data.data);
+}
+const displayPhones = phones => {
+  const phoneContainer = document.getElementById('phone-container');
+  phoneContainer.innerText = '';
+  phones = phones.slice(0,21);
+  const noPhone = document.getElementById('no-phone-found');
+  if(phones.length === 0) {
+    noPhone.classList.remove('d-none')
+  }
+  else{
+    noPhone.classList.add('d-none');
+  }
+  phones.forEach(phone => {
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('col');
+    phoneDiv.innerHTML = `
+        <div class="card p-4">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name
+      }</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+        `;
+    phoneContainer.appendChild(phoneDiv);
+  });
+  toggleSpinners(false);
+}
+
+document.getElementById('btn-search').addEventListener('click', function() {
+  toggleSpinners(true);
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  loadPhones(searchText);
+})
+
+const toggleSpinners = isLoading => {
+  const loaderSection = document.getElementById('toggle-spinners');
+  if(isLoading) {
+    loaderSection.classList.remove('d-none')
+  }
+  else{
+    loaderSection.classList.add('d-none')
+  };
+}
+// loadPhones();
